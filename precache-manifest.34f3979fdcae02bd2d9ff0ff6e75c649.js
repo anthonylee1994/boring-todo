fb490a380d4e771a7e94f31751f072fc4d0c6e3d
Anthony SHERGON <p3.shergonprint@gmail.com>
2021-05-05 13:35:56 +0800

    dist

---
+++ precache-manifest.34f3979fdcae02bd2d9ff0ff6e75c649.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a323c23e8afd2defd7a146a2ad7524",
+    "url": "./icon-144x144.png"
+  },
+  {
+    "revision": "332e5ba33eb7edd3961226cb6196e02f",
+    "url": "./icon-192x192.png"
+  },
+  {
+    "revision": "850568724a92700e09ad11fc40938471",
+    "url": "./icon-48x48.png"
+  },
+  {
+    "revision": "457f9ed0fdd04f1c1cacc9d61c3e849e",
+    "url": "./icon-72x72.png"
+  },
+  {
+    "revision": "473c497e0e7bf506e0f53c537803ae13",
+    "url": "./icon-96x96.png"
+  },
+  {
+    "revision": "84bb661ddf7229a26097af798d69b76e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "62d01e04e9b170e16e98",
+    "url": "./umi.css"
+  },
+  {
+    "revision": "62d01e04e9b170e16e98",
+    "url": "./umi.js"
+  }
+]);
