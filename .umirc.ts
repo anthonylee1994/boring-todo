750abf442138ae6ad6a7ab3847b38e2f337a6c9a
Anthony SHERGON <p3.shergonprint@gmail.com>
2021-05-05 12:20:34 +0800

    fix: manifest.json

--- .umirc.ts
+++ .umirc.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  base: "./",
+  publicPath: "./",
   exportStatic: {
     htmlSuffix: true,
     dynamicRoot: true,
   },
   pwa: {
-    src: "manifest.json",
+    src: "./manifest.json",
     hash: true,
   },
   locale: {
@@ -16,6 +18,7 @@ export default defineConfig({
   nodeModulesTransform: {
     type: "none",
   },
+  title: "\u60b6\u4e8b\u9805\u6e05\u55ae",
   fastRefresh: {},
   theme: {
     "primary-color": "#faad14",
